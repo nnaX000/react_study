2a28225d36647f56d7199210d3c55f0b19c5d9c2
김나영 <kny200208@naver.com>
2024-12-12 22:55:40 +0900

    1.5) 자료형

---
+++ chapter05.js
@@ -0,0 +1,38 @@
+//1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+// console.log(num1+num2);
+// console.log(num1-num2);
+// console.log(num1*num2);
+// console.log(num1/num2);
+// console.log(num1%num2);
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+//console.log(1*"hello"); 이런 불가능한 명령도 콘솔에 Nan이라고 찍힘.
+
+//2. String type
+let myName = "이정환";
+let myLocation = "목동";
+let introduce = myName+myLocation;
+
+// console.log(introduce);
+
+// let introduceText = `${myName}은 ${myLocation}에 거주합니다.`
+// console.log(introduceText);
+
+//3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+//4. Null type(아무것도 없다)
+let empty=null;
+
+//5. Undefined type
+let none;
+console.log(none);
